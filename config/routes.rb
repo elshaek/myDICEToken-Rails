8ac468cec5d0c95258050136671004f4a5cc9631
Elsha Kwee <elsha.e.k@gmail.com>
2017-11-27 23:52:36 +0700

    ADD routes for token details and different currencies

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/currencies/USD', to: 'currencies#usd'
+  get '/currencies/IDR', to: 'currencies#idr'
+  get '/token/details', to: 'tokens#details'
+
+  root to: 'currencies#usd'
 end
