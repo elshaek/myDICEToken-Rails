e70a930091208f1ee692974c09e410962e2b317b
Elsha Kwee <elsha.e.k@gmail.com>
2017-11-28 02:15:59 +0700

    ADD api request

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -1,5 +1,19 @@
+require 'uri'
+require 'net/http'
+
 class TokensController < ApplicationController
   def details
+    url = URI("https://min-api.cryptocompare.com/data/price?fsym=DICE&tsyms=ETH%2CUSD%2CIDR")
 
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(url)
+
+    response = JSON.parse(http.request(request).read_body)
+    @eth = response['ETH']
+    @usd = response['USD']
+    @idr = response['IDR']
   end
 end
