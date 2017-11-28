1547e1b3f1bb0417a3274548a379c0d07794e3e1
Elsha Kwee <elsha.e.k@gmail.com>
2017-11-28 20:07:35 +0700

    ADD request_info method that returns json when called

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -1,9 +1,20 @@
 class CurrenciesController < ApplicationController
-  def usd
+  include CurrenciesHelper
 
+  def usd
+    @ethusd = eth_usd
   end
 
   def idr
+    @ethidr = eth_idr
+  end
 
+  def request_info
+    @ethidr_rate = usd
+    @ethusd_rate = idr
+
+    respond_to do |format|
+      format.json { render json: {ethidr_rate: @ethidr_rate, ethusd_rate: @ethusd_rate}}
+    end
   end
 end
